196442ab7c24eed957770cf726966dfe02634f1a
Patrick Gunderson <patrick@toolofna.com>
2016-12-10 16:31:27 -0800

    changed pre-rendered wave names

--- src/math/Constants.js
+++ src/math/Constants.js
@@ -7,6 +7,6 @@ export const TAU = PI * 2;
 export const PHI = 1.6180339887498948482;
 export const e = 2.71828182845904523536;
 
-export const SIN = sampleFn( Math.sin, TAU, 1000 );
-export const COS = sampleFn( Math.cos, TAU, 1000 );
-export const TAN = sampleFn( Math.tan, TAU, 1000 );
+export const SIN_WAVE = sampleFn( Math.sin, TAU, 1000 );
+export const COS_WAVE = sampleFn( Math.cos, TAU, 1000 );
+export const TAN_WAVE = sampleFn( Math.tan, TAU, 1000 );
